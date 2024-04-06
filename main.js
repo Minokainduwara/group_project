c6db7fae4cc6451f972ad13040e8a77c8669dd2b
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2024-04-06 15:56:21 +0530

    make the design responsive

--- main.js
+++ main.js
@@ -2,7 +2,23 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () =>{
     search.classList.toggle('active');
+    menu.classList.remove('active');
 };
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+};
+
+//Hide Menu and Search Box on Scroll
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
+
+//Sticky mavbar
 
 window.addEventListener('scroll', function() {
     let header = document.querySelector("header");
