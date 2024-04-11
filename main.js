0434a9ce138d56cebb098c910f3cceb7a78bf3f5
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2024-04-11 16:36:43 +0530

    final

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Search bar
+
 let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () =>{
@@ -24,3 +26,32 @@ window.addEventListener('scroll', function() {
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+//Contact us form validation
+function contact_message(){
+    let name = document.forms["myForm"]["name_field"].value;
+    let email = document.forms["myForm"]["email_field"].value;
+    let subject = document.forms["myForm"]["subject_field"].value;
+    let message = document.forms["myForm"]["message_field"].value;
+
+    if (name == "") {
+        alert("Enter Your Name");
+        return false;
+    } 
+    else if(email == ""){
+        alert("Enter Your Email");
+        return false;
+    }
+    else if(subject == ""){
+        alert("Enter Your Subject");
+        return false;
+    }
+    else if (message == ""){
+        alert("Enter Your Message");
+        return false;
+    }
+    else{
+        alert(name + " Your Message Recieved ");
+        return false;
+    }
+}
